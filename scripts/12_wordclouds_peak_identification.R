ae8debc709df2bc226d9f9b0a84d80bd055537dd
Hannah Metzler <hannahmetzler1@gmail.com>
2021-12-05 12:41:40 +0100

    content is part of script 09

--- scripts/12_wordclouds_peak_identification.R
+++
@@ -1,9 +0,0 @@
-#wordclouds to identify peaks per category
-#libraries
-library(dplyr)
-library(lubridate)
-library(stringr)
-library(beepr) # to beep after a long analysis is finished
-library(wordcloud)
-library(tidytext) #for making word list with frequencies
-
